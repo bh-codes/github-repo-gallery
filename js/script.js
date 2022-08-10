64258dff1470e5daf4ebfa5348e6f31b70be6395
bet_h <bettybhan@gmail.com>
2022-08-10 13:15:59 -0400

    Displays repos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // div where profile information will appear
 const overview = document.querySelector(".overview");
 const username = "bh-codes";
+// ul to display the repos list
+const repoList = document.querySelector(".repo-list");
 
 const profileInfo = async function() {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -22,4 +24,22 @@ const displayInfo = function(data) {
     <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
   </div> `;
   overview.append(div);
+
+  repos();
+};
+
+const repos = async function() {
+    const getRepo = await fetch (`https://api.github.com/users/${username}/repos`);
+    const data = await getRepo.json();
+    console.log(data)
+    displayRepo(data);
+};
+
+const displayRepo = function(repo) {
+    for (const item of repo) {
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${item.name}</h3>`;
+        repoList.append(li);
+    };
 };
